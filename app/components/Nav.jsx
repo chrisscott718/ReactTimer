918e12671b64b07e63d03a1ee2bbe89115d5bacb
Chris Scott <chris.scott718@gmail.com>
2017-05-23 18:20:47 -0700

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return(
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="dropdown menu" data-dropdown-menu>
+          <li className="menu-text">React Timer App</li>
+          <li><IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
+          <li><Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="https://www.github.com/chrisscott718" target="_blank">Chris Scott</a></li>
+          </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
